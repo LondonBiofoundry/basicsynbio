bc51ffbfb73f43139b4dc5b39d5a571b5c3101a4
Matthew Haines <hainesm6@gmail.com>
2020-02-24 19:10:42 +0000

    save tests

---
+++ tests/test_basicsynbio.py
@@ -0,0 +1,26 @@
+# Uses pytest
+import basicsynbio
+import pytest
+from Bio import SeqRecord
+from Bio import SeqIO
+
+
+@pytest.fixture
+def generic_basic_part():
+    return basicsynbio.import_basic_part("generic_basic_part.gb", "genbank")
+
+
+@pytest.fixture
+def generic_seqrec():
+    return SeqIO.read("generic_basic_part.gb", "genbank")
+
+
+def test_basic_part(generic_basic_part, generic_seqrec):
+    assert generic_basic_part.seq == generic_seqrec.seq
+    assert generic_basic_part.id == generic_seqrec.id
+
+
+def test_basic_slice_ip_first(generic_basic_part):
+    sliced_part = generic_basic_part.basic_slice()
+    presliced_part = SeqIO.read("sliced_basic_part_ip.gb", "genbank")
+    assert sliced_part.seq == presliced_part.seq
