6b0c2e1c528da20a4bae4e04d6f10daef93be3a5
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2021-01-15 20:19:47 +0000

    changes 'temp_id' to name of linker and adds a test with black formatting

--- basicsynbio/parts_linkers/basic_linkers.py
+++ basicsynbio/parts_linkers/basic_linkers.py
@@ -99,7 +99,7 @@ def _make_linker(
         object of type linker_class specified in Args.
     """
     seq = Seq("GG" + str_seq)
-    linker = linker_class(seq, id="temp_id", name=name, description=description)
+    linker = linker_class(seq, id=name, name=name, description=description)
     linker.id = seqrecord_hexdigest(linker)
     return linker
 
