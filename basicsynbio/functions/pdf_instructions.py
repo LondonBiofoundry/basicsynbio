83fcc0fdbed35abe2ab1c1c8e56863becfa23be8
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2021-03-31 11:44:10 +0100

    adds pdf path

--- basicsynbio/functions/pdf_instructions.py
+++ basicsynbio/functions/pdf_instructions.py
@@ -34,7 +34,9 @@ from basicsynbio.utils import (
 )
 
 
-def pdf_instructions(basic_build: BasicBuild, assemblies_per_clip: int = 28):
+def pdf_instructions(
+    basic_build: BasicBuild, path: str = None, assemblies_per_clip: int = 28
+):
     """Writes information about each clip_data and assembly to
     two dependent CSV files in the same folder the command
     is executed.
@@ -116,7 +118,14 @@ def pdf_instructions(basic_build: BasicBuild, assemblies_per_clip: int = 28):
     CLIPS_DATA = clips_data_from_pandas(clips_data)
     ASSEMBLIES_DATA = assembly_data_from_pandas(assemeblies_data)
 
-    pdf_filename = f"pdf_{datetime.now().strftime('%d-%m-%Y_%H.%M.%S')}.pdf"
+    if path == None:
+        now = datetime.now()
+        pdf_filename = str(
+            Path.cwd() / f"Echo_Instructions_{now.strftime('%d-%m-%Y_%H.%M.%S')}.zip"
+        )
+    else:
+        pdf_filename = path
+
     pdf = SimpleDocTemplate(pdf_filename, pagesizes=A4)
 
     elems = []
@@ -413,4 +422,4 @@ def pdf_instructions(basic_build: BasicBuild, assemblies_per_clip: int = 28):
     )
     pdf.build(elems, canvasmaker=PageNumCanvas)
 
-    return Path.cwd() / pdf_filename
+    return pdf_filename
